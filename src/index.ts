c676ef5cf0aa5fe0eb5740e5a5e006f14bfc4be7
Nan_Ting <a3380991070@163.com>
2026-02-06 20:57:00 +0800

    vite build 打包后压缩 可被napcat加载

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+﻿import type { NapCatPluginContext } from 'napcat-types/napcat-onebot/network/plugin-manger';
+
+import { onMessage } from './消息处理'
+function plugin_init(ctx: NapCatPluginContext) : void {
+    ctx.logger.info("111");
+}
+
+export { plugin_init };
+export const plugin_onmessage  = onMessage;
---
+++ src/消息处理.ts
@@ -0,0 +1,7 @@
+﻿import { OB11Message } from 'napcat-types';
+import type { NapCatPluginContext } from "napcat-types/napcat-onebot/network/plugin-manger";
+
+function onMessage (ctx : NapCatPluginContext, event : OB11Message){
+
+}
+export { onMessage }
